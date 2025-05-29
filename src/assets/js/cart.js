9a62643a469c498ecabef890a6a017ebf5a10bff
stealbe <paradovskiialex@gmail.com>
2025-05-29 22:42:24 +0300

    cart + order (cart object)

---
+++ src/assets/js/cart.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const decrease_quantity = document.querySelectorAll(".decrease_quantity");
+    const increace_quantity = document.querySelectorAll(".increace_quatity");
+});
